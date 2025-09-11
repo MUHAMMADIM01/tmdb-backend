18c66f83d2808682fcd0e4fc39b41e02e6995168
MIMUHAMMADU <M.I.Muhammadribah@gmail.com>
2025-09-11 15:12:36 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from "./pages/Home";
 import Movies from "./pages/Movies";
 import MovieDetail from "./pages/MovieDetail";
 import Favorites from "./pages/Favorites";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path="/movies" element={<Movies />} />
         <Route path="/movies/:id" element={<MovieDetail />} />
         <Route path="/favorites" element={<Favorites />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
