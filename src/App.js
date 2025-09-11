2e7d7aaf55ffe9d79f59b5dac337dc94cf4e4f7c
MIMUHAMMADU <M.I.Muhammadribah@gmail.com>
2025-09-11 11:43:40 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 import Movies from "./pages/Movies";
 import MovieDetail from "./pages/MovieDetail";
 import Favorites from "./pages/Favorites";
-// Home page, connect
-// import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
-      <Navbar /> {/* Navbar to Movies Page */}
+      <Navbar />
       <Routes>
-        {/* Movies list page */}
+        <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
-
-        {/* Single movie detail */}
         <Route path="/movies/:id" element={<MovieDetail />} />
-
-        {/* Favorites page */}
         <Route path="/favorites" element={<Favorites />} />
-        {/* Homepage */}
-        {/* <Route path="/" element={<Home />} /> */}
       </Routes>
     </Router>
   );
