0dc741e332a10238c11ad228a08a67137b12c5ee
MIMUHAMMADU <M.I.Muhammadribah@gmail.com>
2025-09-14 14:45:58 +0100

    Update Client.js

--- src/supabase/Client.js
+++ src/supabase/Client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = "https://kmrszvmzubhhhngejfps.supabase.co";
-const supabaseAnonKey = "YOUR_ANON_KEY";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImttcnN6dm16dWJoaGhuZ2VqZnBzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc2Nzg1OTQsImV4cCI6MjA3MzI1NDU5NH0._hHXWfnAVRRGXj572-qaXJQfo2dASykoiodvOtDLGzc";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
